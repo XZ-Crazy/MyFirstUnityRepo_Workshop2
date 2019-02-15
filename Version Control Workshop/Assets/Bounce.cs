70e9e2aafb615b83d8647a9eba24a282a13a7474
XZ-Crazy <pallittoxz@gmail.com>
2019-02-15 13:51:50 -0800

    Working Bounce Method

---
+++ Version Control Workshop/Assets/Bounce.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(0.0f, 500.0f, 0.0f));
+        //Debug.Log("Yay!");
+
+    }
+}
